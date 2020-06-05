4fefbd50d41d8957da715bb4489652e93aabd149
Enid <enid.soto@gmail.com>
2020-06-05 16:02:45 -0400

    Updates

---
+++ precache-manifest.48cf4ab4ee31caddc8042f7a1f1b8fa2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eed0f6a88037d20ef0ac48cbfffac34",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "4cb9b083872cfe4ed3c7",
+    "url": "/react-portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7fe96de1a106faf333b5",
+    "url": "/react-portfolio/static/css/main.3a3d9fc4.chunk.css"
+  },
+  {
+    "revision": "4cb9b083872cfe4ed3c7",
+    "url": "/react-portfolio/static/js/2.5696a108.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-portfolio/static/js/2.5696a108.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fe96de1a106faf333b5",
+    "url": "/react-portfolio/static/js/main.9a264213.chunk.js"
+  },
+  {
+    "revision": "bee14ebf850387594d39",
+    "url": "/react-portfolio/static/js/runtime-main.8c64cdeb.js"
+  },
+  {
+    "revision": "3e3605a2c4c1a9092f12a8a4b9254917",
+    "url": "/react-portfolio/static/media/header-graphic-01.3e3605a2.svg"
+  },
+  {
+    "revision": "413d6ddd53c9e5549a6d0b92008f4d2d",
+    "url": "/react-portfolio/static/media/header-graphic-02.413d6ddd.svg"
+  },
+  {
+    "revision": "6096e950593acf1fb69b4fd9cce99e66",
+    "url": "/react-portfolio/static/media/header-graphic-03.6096e950.svg"
+  }
+]);
